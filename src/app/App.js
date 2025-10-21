633cf136a616c74f4a5564d2ef3a4eb907499e42
Kimchhay Leng <150762525+ChhayZ7@users.noreply.github.com>
2025-10-21 19:31:42 +1030

    Add main function to run the app

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,12 @@
 import '../assets/cors-redirect';
-import './App.css';
+import '../assets/App.css';
 // import { initStrudel, note, hush, evalScope, getAudioContext, webaudioOutput, registerSynthSounds, initAudioOnFirstClick, transpiler } from "@strudel/web";
 import { useEffect, useRef, useMemo, useState } from "react";
 // import { StrudelMirror } from '@strudel/codemirror';
 // import { registerSoundfonts } from '@strudel/soundfonts';
-import { useStrudleEditor } from "../hooks/useStrudleEditor"
+import { useStrudelEditor } from "../hooks/useStrudelEditor"
 import { stranger_tune } from './tunes';
+import PreprocessInput from '../components/preprocessInput';
 
 // let globalEditor = null;
 
@@ -15,7 +16,51 @@ function preprocess(text, hush){
   return text.replaceAll("<p1_Radio>", replacement);
 }
 
+export default function App(){
+  const { mountRef, ready, setCode, evaluate, stop, isStarted } = useStrudelEditor();
 
+  const [rawText, setRawText] = useState("");
+  const [hush, setHush] = useState(false);
+
+  const processed = useMemo(() => preprocess(rawText, hush), [rawText, hush]);
+
+  useEffect(() => {
+    if(ready){
+      setRawText(stranger_tune);
+      // Wait until the editor is mounted before setting code
+      queueMicrotask(() => setCode(preprocess(stranger_tune, false)))
+    }
+  }, [ready, setCode]);
+
+  useEffect(() => {
+    if(!ready) return;
+    setCode(processed);
+    if(isStarted()) evaluate();
+  }, [hush, processed, ready, setCode, evaluate, isStarted])
+
+  const handlePreprocess = () => {
+    if (!ready) return;
+    setCode(processed);
+  }
+
+  const handleProcPlay = () => {
+    if (!ready) return;
+    setCode(processed);
+    evaluate();
+  }
+  // Render UI layout
+  return (
+    <div>
+    <h2>Strudle Demo</h2>
+    <main class="container-fluid">
+      {/* Top section text input and transport buttons */}
+      <div class="row">
+        <PreprocessInput value={rawText} onChange={setRawText}/>
+      </div>
+    </main>
+  </div>
+  )
+}
 
 // export function SetupButtons() {
 
