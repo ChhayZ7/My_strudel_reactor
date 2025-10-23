163ff901cda4b0683b4103903838c5f98552258c
Kimchhay Leng <150762525+ChhayZ7@users.noreply.github.com>
2025-10-23 21:51:49 +1030

    Implement transport buttons component

--- src/components/transport.js
+++ src/components/transport.js
@@ -8,8 +8,15 @@ function Transport({
     disabled = false,
 }){
     return (
-        <div>
-            Transport
+        <div class="col-md-4">
+            <nav class="d-grid gap-2">
+                <button onClick={onPreprocess} disabled={disabled}>Preprocess</button>
+                <button onClick={onProcPlay} disabled={disabled}>Proc & Play</button>
+                <button onClick={onPlay} disabled={disabled}>Play</button>
+                <button onClick={onStop} disabled={disabled}>Stop</button>
+            </nav>
         </div>
     )
 }
+
+export default Transport;
