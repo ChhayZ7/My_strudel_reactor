96d9e4abc67f88083d5426e5071e499d57b8d2ef
Kimchhay Leng <150762525+ChhayZ7@users.noreply.github.com>
2025-10-27 19:42:38 +1030

    Implement hushToggles

--- src/components/hushToggle.js
+++ src/components/hushToggle.js
@@ -1,9 +1,34 @@
 import React from "react";
 
-function HushToggle(){
+function HushToggle({hush, onChange}){
     return (
-        <div>
-            Hush Toggle
+        <div className="col-md-4">
+            <div className="form-check">
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    name="flexRadioDefault"
+                    id="flexRadioDefault1"
+                    onChange={() => onChange(false)}
+                    checked={!hush}
+                />
+                <label className="form-check-label" htmlFor="p1_on">
+                    p1: ON
+                </label>
+            </div>
+            <div className="form-check">
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    name="flexRadioDefault"
+                    id="flexRadioDefault2"
+                    onChange={() => onChange(true)}
+                    checked={hush}
+                />
+                <label className="form-check-label" htmlFor="p1_hush">
+                    p1: HUSH
+                </label>
+            </div>
         </div>
     )
 }
