c16fb4d5770ed8060a1312eb0ea0c02871903b48
Kimchhay Leng <150762525+ChhayZ7@users.noreply.github.com>
2025-11-02 17:17:30 +1030

    Create strudelPreprocessing.js
    - Add function to detect instrument parts in a unprocessed tune code
    - Add function to preprocess tune

---
+++ src/utils/strudelPreprocessing.js
@@ -0,0 +1,11 @@
+// Helper functions
+
+// Extract all parts from the tune's code
+export function detectParts(tune){
+
+}
+
+// Preprocess code based on part states
+export function preprocess(text, partState){
+
+}
