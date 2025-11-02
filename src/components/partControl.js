c395da5265c47b5370f71554d6d0db43b16b6899
Kimchhay Leng <150762525+ChhayZ7@users.noreply.github.com>
2025-11-02 15:16:35 +1030

    Implement control buttons (On,Hush, Solo) for instrument part such as Arpeggio, bassline, drum

--- src/components/partControl.js
+++ src/components/partControl.js
@@ -1,7 +1,56 @@
-function PartControl (){
+function PartControl ({ partName, state, onChange, disabled }){
+    const displayName = partName.charAt(0).toUpperCase() + partName.slice(1);
+
     return (
         <div>
-            <h5>Part Control</h5>
+            <div>
+                <h6>
+                    {displayName === 'Arp' ? 'PianoEmoji' : displayName === 'Bassline' ? 'BassEmoji' : 'DrumEmoji'} {displayName}:
+                </h6>
+                <div>
+                    <input
+                        type="radio"
+                        className="btn-check"
+                        name={`part_${partName}`}
+                        id={`${partName}_on`}
+                        autoComplete="off"
+                        checked={state === 'on'}
+                        onChange={() => onChange(partName, 'on')}
+                        disabled={disabled}
+                    />
+                    <label className="btn btn-outline-success btn-sm" htmlFor={`${partName}_on`}>
+                        ON
+                    </label>
+
+                    <input
+                        type="radio"
+                        className="btn-check"
+                        name={`part_${partName}`}
+                        id={`${partName}_hush`}
+                        autoComplete="off"
+                        checked={state === 'hush'}
+                        onChange={() => onChange(partName, 'hush')}
+                        disabled={disabled}
+                    />
+                    <label  className="btn btn-outline-success btn-sm" htmlFor={`${partName}_hush`}>
+                        HUSH
+                    </label>
+
+                    <input
+                        type="radio"
+                        className="btn-check"
+                        name={`part_${partName}`}
+                        id={`${partName}_solo`}
+                        autoComplete="off"
+                        checked={state === 'solo'}
+                        onChange={() => onChange(partName, 'solo')}
+                        disabled={disabled}
+                    />
+                    <label  className="btn btn-outline-warning btn-sm" htmlFor={`${partName}_hush`}>
+                        SOLO
+                    </label>
+                </div>
+            </div>
         </div>
     )
 }
