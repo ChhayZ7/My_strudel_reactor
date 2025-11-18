b823724bfe31b1732653bc6c6e28ef1b9ca7179f
ChhayZ7 <150762525+ChhayZ7@users.noreply.github.com>
2025-11-18 19:28:34 +1030

    Add emoji to style transport control buttons

--- src/components/transport.js
+++ src/components/transport.js
@@ -10,10 +10,10 @@ function Transport({
 }){
     return (
             <nav className="d-grid gap-2">
-                <button className="btn btn-outline-primary" onClick={onPreprocess} disabled={disabled}>Preprocess</button>
-                <button className="btn btn-outline-primary" onClick={onProcPlay} disabled={disabled}>Proc & Play</button>
-                <button className="btn btn-outline-primary" onClick={onPlay} disabled={disabled}>Play</button>
-                <button className="btn btn-outline-primary" onClick={onStop} disabled={disabled}>Stop</button>
+                <button className="btn btn-outline-primary" onClick={onPreprocess} disabled={disabled}>⚙️Preprocess</button>
+                <button className="btn btn-outline-primary" onClick={onProcPlay} disabled={disabled}>▶️⚙️Proc & Play</button>
+                <button className="btn btn-outline-primary" onClick={onPlay} disabled={disabled}>▶️Play</button>
+                <button className="btn btn-outline-primary" onClick={onStop} disabled={disabled}>⏹️Stop</button>
             </nav>
     )
 }
